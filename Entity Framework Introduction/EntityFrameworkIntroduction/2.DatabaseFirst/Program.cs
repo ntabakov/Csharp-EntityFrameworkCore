0657b27af04e4b314d141803aa6ba2b2c2b88411
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2021-02-24 14:30:38 +0200

    add homework

---
+++ Entity Framework Introduction/EntityFrameworkIntroduction/2.DatabaseFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2.DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
