afabe14fa92e1e0b48da0e1262e4ceb0835d2a1d
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2021-02-25 12:58:00 +0200

    add new homework

---
+++ Entity Framework Introduction/EntityFrameworkIntroduction/SoftUni/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+            string result = GetEmployeesFullInformation(db);
+            Console.WriteLine(result);
+
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees.Select(x => new
+                {
+                    x.EmployeeId,
+                    x.FirstName,
+                    x.LastName,
+                    x.MiddleName,
+                    x.JobTitle,
+                    x.Salary
+                })
+                .OrderBy(x => x.EmployeeId);
+
+            var sb = new StringBuilder();
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+    }
+}
