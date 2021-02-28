9432876b67c7e7a2c6324ecdb6e61e6a96013ce0
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2021-02-28 22:09:11 +0200

    add homework

---
+++ Entity Relations/P01_StudentSystem/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "char")]
+        [MaxLength(10)]
+        public string PhoneNumber { get; set; }
+
+        public DateTime RegisteredOn { get; set; }
+        public DateTime? Birthday { get; set; }
+    }
+}
