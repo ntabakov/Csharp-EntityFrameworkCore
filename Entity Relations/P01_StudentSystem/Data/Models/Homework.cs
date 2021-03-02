c0d1f6571d8fd1e2dbe63bd3d39a324f812b376d
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2021-03-02 20:59:02 +0200

    add

--- Entity Relations/P01_StudentSystem/Data/Models/Homework.cs
+++ Entity Relations/P01_StudentSystem/Data/Models/Homework.cs
@@ -8,7 +8,7 @@ namespace P01_StudentSystem.Data.Models
     {
         public int HomeworkId { get; set; }
         [Required]
-        [Column(TypeName = "varchar(255")]
+        [Column(TypeName = "varchar(255)")]
         public string Content { get; set; }
         [Required]
         public ContentType ContentType { get; set; }
