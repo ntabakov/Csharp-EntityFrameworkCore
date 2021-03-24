1905c7bc8c9959b6e0d4c166ab3294944d6234c5
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2021-03-24 21:03:53 +0200

    add

---
+++ XML Processing/ProductShop/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var db = new ProductShopContext();
+            //db.Database.EnsureDeleted();
+            //db.Database.EnsureCreated();
+            var xml = File.ReadAllText("../../../Datasets/users.xml");
+            Console.WriteLine(ImportUsers(db,xml));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(UsersDTO[]), 
+                new XmlRootAttribute("Users"));
+
+            var xmlRead = new StringReader(inputXml);
+
+            var usersDTO = xmlSerializer.Deserialize(xmlRead) as UsersDTO[];
+
+            var users = usersDTO.Select(x => new User
+            {
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Age = x.Age,
+            }).ToList();
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+    }
+}
