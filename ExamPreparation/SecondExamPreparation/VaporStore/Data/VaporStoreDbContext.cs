c13388d3d51c2efead233b0596634f477dee3c65
Nedyalko Tabakov <nedqlkotabakov00@gmail.com>
2021-04-01 16:52:29 +0300

    add

---
+++ ExamPreparation/SecondExamPreparation/VaporStore/Data/VaporStoreDbContext.cs
@@ -0,0 +1,36 @@
+﻿using VaporStore.Data.Models;
+
+namespace VaporStore.Data
+{
+	using Microsoft.EntityFrameworkCore;
+
+	public class VaporStoreDbContext : DbContext
+	{
+		public VaporStoreDbContext()
+		{
+		}
+
+		public VaporStoreDbContext(DbContextOptions options)
+			: base(options)
+		{
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			if (!options.IsConfigured)
+			{
+				options
+					.UseSqlServer(Configuration.ConnectionString);
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<GameTag>().HasKey(x => new
+            {
+                x.GameId,
+                x.TagId,
+            });
+        }
+	}
+}
